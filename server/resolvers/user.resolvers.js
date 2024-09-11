503631b32f2c3cd61bcfdaebb373fe33a1632ad9
aniket <aniketkoli17884@gmail.com>
2024-09-11 14:14:44 +0530

    Add transcation resolvers

--- server/resolvers/user.resolvers.js
+++ server/resolvers/user.resolvers.js
@@ -1,7 +1,74 @@
-import User from '../models/user.model.js'
+import Transaction from "../models/transaction.model.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
 const userResolver = {
-    Query: {
+	Mutation: {
+		signUp: async (_, { input }, context) => {
+			try {
+				const { username, name, password, gender } = input;
+
+				if (!username || !name || !password || !gender) {
+					throw new Error("All fields are required");
+				}
+				const existingUser = await User.findOne({ username });
+				if (existingUser) {
+					throw new Error("User already exists");
+				}
+
+				const salt = await bcrypt.genSalt(10);
+				const hashedPassword = await bcrypt.hash(password, salt);
+
+				// https://avatar-placeholder.iran.liara.run/
+				const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+				const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+				const newUser = new User({
+					username,
+					name,
+					password: hashedPassword,
+					gender,
+					profilePicture: gender === "male" ? boyProfilePic : girlProfilePic,
+				});
+
+				await newUser.save();
+				await context.login(newUser);
+				return newUser;
+			} catch (err) {
+				console.error("Error in signUp: ", err);
+				throw new Error(err.message || "Internal server error");
+			}
+		},
+
+		login: async (_, { input }, context) => {
+			try {
+				const { username, password } = input;
+				if (!username || !password) throw new Error("All fields are required");
+				const { user } = await context.authenticate("graphql-local", { username, password });
+
+				await context.login(user);
+				return user;
+			} catch (err) {
+				console.error("Error in login:", err);
+				throw new Error(err.message || "Internal server error");
+			}
+		},
+		logout: async (_, __, context) => {
+			try {
+				await context.logout();
+				context.req.session.destroy((err) => {
+					if (err) throw err;
+				});
+				context.res.clearCookie("connect.sid");
+
+				return { message: "Logged out successfully" };
+			} catch (err) {
+				console.error("Error in logout:", err);
+				throw new Error(err.message || "Internal server error");
+			}
+		},
+	},
+	Query: {
 		authUser: async (_, __, context) => {
 			try {
 				const user = await context.getUser();
@@ -21,58 +88,17 @@ const userResolver = {
 			}
 		},
 	},
-	
-    Mutation :{
-        signup: async (_,{input},context) => { // context is from expressMiddleware
-            try {
-                const {username , name , password , gender} = input
-
-                if ( !username || !name || !password || !gender){
-                    throw new Error('All fields are required.')
-                }
-
-                const exstingUser = await User.findOne({ username})
-                if (existingUser){
-                    throw new Error('Username already exists.')
-                }
-
-                const hashedPassword = await bcrypt.hash(password , 10)
-
-                const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
-				const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
-                
-                const newUser = new User({
-                    username,
-                    name,
-                    password: hashedPassword,
-                    profilePicture: gender === 'male'? boyProfilePic : girlProfilePic,
-                    gender
-                })
-
-                await newUser.save()
-                await context.login(newUser)
-                return newUser
-            } catch (error) {
-                throw new Error(error.message || "Internal server error")
-                console.log("Error in signUp: " , error)
-            }
-        },
-
-        logout: async (_, __, context) => {
+	User: {
+		transactions: async (parent) => {
 			try {
-				await context.logout();
-				context.req.session.destroy((err) => {
-					if (err) throw err;
-				});
-				context.res.clearCookie("connect.sid");
-
-				return { message: "Logged out successfully" };
+				const transactions = await Transaction.find({ userId: parent._id });
+				return transactions;
 			} catch (err) {
-				console.error("Error in logout:", err);
+				console.log("Error in user.transactions resolver: ", err);
 				throw new Error(err.message || "Internal server error");
 			}
 		},
-    } 
-}
+	},
+};
 
 export default userResolver;
