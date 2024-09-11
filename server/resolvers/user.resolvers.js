f9c20d01d1eacd3f780e71150658514bda3ee78b
aniket <aniketkoli17884@gmail.com>
2024-09-11 14:05:50 +0530

    Add user resolver

--- server/resolvers/user.resolvers.js
+++ server/resolvers/user.resolvers.js
@@ -1,16 +1,78 @@
-import  { users } from '../dummyData/data.js'
+import User from '../models/user.model.js'
 
 const userResolver = {
-    Query :{
-        users : () => {
-            return users
+    Query: {
+		authUser: async (_, __, context) => {
+			try {
+				const user = await context.getUser();
+				return user;
+			} catch (err) {
+				console.error("Error in authUser: ", err);
+				throw new Error("Internal server error");
+			}
+		},
+		user: async (_, { userId }) => {
+			try {
+				const user = await User.findById(userId);
+				return user;
+			} catch (err) {
+				console.error("Error in user query:", err);
+				throw new Error(err.message || "Error getting user");
+			}
+		},
+	},
+	
+    Mutation :{
+        signup: async (_,{input},context) => { // context is from expressMiddleware
+            try {
+                const {username , name , password , gender} = input
+
+                if ( !username || !name || !password || !gender){
+                    throw new Error('All fields are required.')
+                }
+
+                const exstingUser = await User.findOne({ username})
+                if (existingUser){
+                    throw new Error('Username already exists.')
+                }
+
+                const hashedPassword = await bcrypt.hash(password , 10)
+
+                const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+				const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+                
+                const newUser = new User({
+                    username,
+                    name,
+                    password: hashedPassword,
+                    profilePicture: gender === 'male'? boyProfilePic : girlProfilePic,
+                    gender
+                })
+
+                await newUser.save()
+                await context.login(newUser)
+                return newUser
+            } catch (error) {
+                throw new Error(error.message || "Internal server error")
+                console.log("Error in signUp: " , error)
+            }
         },
 
-        user : (_, { id }) => {
-            return users.find(user => user.id === id)
-        }
-    } ,
-    Mutation :{} 
+        logout: async (_, __, context) => {
+			try {
+				await context.logout();
+				context.req.session.destroy((err) => {
+					if (err) throw err;
+				});
+				context.res.clearCookie("connect.sid");
+
+				return { message: "Logged out successfully" };
+			} catch (err) {
+				console.error("Error in logout:", err);
+				throw new Error(err.message || "Internal server error");
+			}
+		},
+    } 
 }
 
 export default userResolver;
