a772224ca8eb6e055e75c84e7d83041c49f659c3
aniket <aniketkoli17884@gmail.com>
2024-09-12 11:21:06 +0530

    Add UI Design

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import {Routes , Route} from 'react-router-dom'
+
+import SignUpPage from './pages/SignUpPage';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import TranscationPage from './pages/TranscationPage';
+import NotFoundPage from './pages/NotFoundPage';
+import Header from './components/ui/Header';
+
 import './App.css';
 
+
 function App() {
+  const authUser = true
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-black">
+      {authUser && <Header/>}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/transcation" element={<TranscationPage />} />
+        <Route path="/not-found" element={<NotFoundPage />} />
+        
+      </Routes>
     </div>
   );
 }
